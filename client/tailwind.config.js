eb7bd284c248422d0494aaaae5af1d98862ffb33
Gregory Hawman <g.hawman@gmail.com>
2022-03-16 13:02:30 -0400

    added all the node modules I think I need. set up an init tailwindcss. Removed cra bloat files.

---
+++ client/tailwind.config.js
@@ -0,0 +1,12 @@
+module.exports = {
+  content: [
+    './src/**/*.{js,jsx,ts,tsx}'
+  ],
+  theme: {
+    boxShadow: {
+      'all': '0px 4px 30px 10px rgb(79, 114, 205, 0.15)',
+    },
+    extend: {},
+  },
+  plugins: [],
+}
